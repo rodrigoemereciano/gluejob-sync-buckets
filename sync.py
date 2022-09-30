6079a8617c44361265ced4eb721350469380f1b6
rodrigoemereciano <rodrigo.emereciano@gmail.com>
2022-09-29 22:24:39 -0300

    ajuste script

--- sync.py
+++ sync.py
@@ -4,5 +4,5 @@ import os
 source_bucket = "metricsdev-src-bucket"
 target_bucket = "metricsdev-dst-bucket"
 target_path = "tbff20001_metrica"
-sync_command = f"aws s3 sync s3://$source_bucket/ s3://$target_bucket/$target_path/"
+sync_command = f"aws s3 sync s3://{source_bucket}/ s3://{target_bucket}/{target_path}/"
 os.system(sync_command)
